1e3ceec3396019f51e5f13e7ed291d79769c879d
Rhadow <howard3433@gmail.com>
2016-04-26 20:55:00 +0800

    Use github photo fetcher as index

--- src/Main.elm
+++ src/Main.elm
@@ -9,38 +9,38 @@ import Html exposing (Html)
 -- import Counter exposing (model, update, view)
 
 -- Example 2. Form Validation
-import FormValidator exposing (model, update, view)
+-- import FormValidator exposing (model, update, view)
 
 -- Example 3. Form Validation and Http request
--- import GithubPhotoFetcher exposing (init, update, view)
+import GithubPhotoFetcher exposing (init, update, view)
 
 import Effects exposing (Never)
 
 
 -- For Example 1 & 2
-main: Signal Html
-main =
-    Simple.start
-        { model = model
-        , update = update
-        , view = view
-        }
+-- main: Signal Html
+-- main =
+--     Simple.start
+--         { model = model
+--         , update = update
+--         , view = view
+--         }
 
 
 -- For Example 3
--- app =
---     StartApp.start
---         { init = init
---         , update = update
---         , view = view
---         , inputs = []
---         }
---
--- main: Signal Html
--- main =
---     app.html
---
---
--- port tasks: Signal (Task.Task Never ())
--- port tasks =
---     app.tasks
+app =
+    StartApp.start
+        { init = init
+        , update = update
+        , view = view
+        , inputs = []
+        }
+
+main: Signal Html
+main =
+    app.html
+
+
+port tasks: Signal (Task.Task Never ())
+port tasks =
+    app.tasks
