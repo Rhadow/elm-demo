f70fd2e4ed25e95ce135d0c2b1208af31c50d5ab
Rhadow <howard3433@gmail.com>
2016-04-26 21:18:05 +0800

    Complete Ex1

---
+++ src/Counter.elm
@@ -0,0 +1,33 @@
+module Counter (Model, Action, init, update, view) where
+
+import Html exposing (Html, button, span, text, div)
+import Html.Events exposing (onClick)
+
+
+type alias Model = Int
+
+
+type Action
+    = Increase
+    | Decrease
+
+
+init: Model
+init = 0
+
+
+update: Action -> Model -> Model
+update action model =
+    case action of
+        Increase -> model + 1
+        Decrease -> model - 1
+
+
+view: Signal.Address Action -> Model -> Html
+view address model =
+    div
+        []
+        [ button [onClick address Decrease] [ text "-" ]
+        , span [] [ toString model |> text ]
+        , button [onClick address Increase] [ text "+" ]
+        ]
--- src/Main.elm
+++ src/Main.elm
@@ -1,1 +1,14 @@
 module Main where
+
+import StartApp.Simple
+import Html exposing (Html)
+import Counter exposing (init, update, view)
+
+
+main: Signal Html
+main =
+    StartApp.Simple.start
+        { model = init
+        , update = update
+        , view = view
+        }
